bc80f338eef21d9ba476e07b580c96bba46fdf89
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-09-28 22:55:09 +0200

    added index.html & style.css & index.js & server.js

--- week1/homework/exercises/3-web-server/server.js
+++ week1/homework/exercises/3-web-server/server.js
@@ -3,12 +3,30 @@
  */
 
 var http = require('http');
+const fs = require('fs');
+const { Script } = require('vm');
+
 
 //create a server
 let server = http.createServer(function (req, res) {
   // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+	
+	if(req.url == '/'){
+	const html = fs.readFileSync('index.html')
+	res.writeHead(200, {'Content-Type': 'text/html'})
+	 res.end(html); // Ends the response
+	}
+	 if (req.url == '/index.js') {
+		const js = fs.readFileSync('index.js')
+  	res.writeHead(200, {'Content-Type': 'text/js'})
+	  res.end(js); // Ends the response
+	} 
+	if (req.url == '/style.css') {
+		const css = fs.readFileSync('style.css')
+  	res.writeHead(200, {'Content-Type': 'text/css'})
+	  res.end(css); // Ends the response
+	} 
+
 });
 
-server.listen(3000); // The server starts to listen on port 3000
+server.listen(3000, () => console.log('server is running...')); // The server starts to listen on port 3000
