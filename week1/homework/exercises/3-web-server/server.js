5ba9f2749ca00bcf755d3bee44b49a45fbd8d68e
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-10-03 13:05:25 +0200

    Update server.js

--- week1/homework/exercises/3-web-server/server.js
+++ week1/homework/exercises/3-web-server/server.js
@@ -2,31 +2,33 @@
  * Exercise 3: Create an HTTP web server
  */
 
-var http = require('http');
-const fs = require('fs');
-const { Script } = require('vm');
+var http = require('http')
+const fs = require('fs')
+const { Script } = require('vm')
 
 
 //create a server
-let server = http.createServer(function (req, res) {
+
   // YOUR CODE GOES IN HERE
-	
-	if(req.url == '/'){
+let server = http.createServer(function (req, res) {
+	try{
+	if(req.url === '/'){
 	const html = fs.readFileSync('index.html')
 	res.writeHead(200, {'Content-Type': 'text/html'})
-	 res.end(html); // Ends the response
+	 res.end(html) // Ends the response
 	}
-	 if (req.url == '/index.js') {
+	 if (req.url === '/index.js') {
 		const js = fs.readFileSync('index.js')
-  	res.writeHead(200, {'Content-Type': 'text/js'})
-	  res.end(js); // Ends the response
+  	res.writeHead(200, {'Content-Type': 'application/javascript'})
+	  res.end(js) // Ends the response
 	} 
-	if (req.url == '/style.css') {
+	if (req.url === '/style.css') {
 		const css = fs.readFileSync('style.css')
   	res.writeHead(200, {'Content-Type': 'text/css'})
-	  res.end(css); // Ends the response
+	  res.end(css) // Ends the response
 	} 
+	} catch (err) {console.log('something went wrong' + err)}
 
 });
 
-server.listen(3000, () => console.log('server is running...')); // The server starts to listen on port 3000
+server.listen(3000, () => console.log('server is running...')) // The server starts to listen on port 3000
