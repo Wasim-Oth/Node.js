732f13fdbd56284601849ba0969daebb70707666
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-10-05 13:42:32 +0200

    updated server.js

--- week2/homework/exercises/1-blog-API/server.js
+++ week2/homework/exercises/1-blog-API/server.js
@@ -1,10 +1,55 @@
 const express = require('express')
-const app = express();
+const fs = require("fs");
+// const { title } = require('process');
+const app = express()
+app.use(express.json());
  
 
 // YOUR CODE GOES IN HERE
-app.get('/', function (req, res) {
-  res.send('Hello World')
+
+function error(res){
+  res.status(404)
+  res.send('This post does not exist!')
+}
+
+app.post('/blogs', (req, res) => {
+   title = req.body.title
+   content = req.body.content
+  res.setHeader("Content-Type", "application/json")
+  fs.writeFileSync(title, content);
+  res.send('ok')
+})
+
+app.put('/post/:title', (req, res) => {
+    title = req.body.title
+    content= req.body.content
+if ( fs.existsSync(title)){
+      res.setHeader("Content-Type", "application/json")
+      fs.writeFileSync(title, content);
+      res.end('ok')
+    } else {
+      error(res)
+    }
+})
+
+app.delete('/blogs/:title', (req, res) =>{
+    title = req.params.title
+  if ( fs.existsSync(title)){
+    fs.unlinkSync(title)
+    res.send('ok')
+  } else {
+    error(res)
+  }
+})
+
+app.get('/blogs/:title', (req, res) =>{
+   title = req.params.title
+ if ( fs.existsSync(title)){
+   res.setHeader("Content-Type", "application/json")
+   res.send(fs.readFileSync(title))
+ } else {
+  error(res)
+ }
 })
  
 app.listen(3000)
